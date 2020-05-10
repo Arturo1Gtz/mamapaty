d64eb01bc6f4e336a12943feaec6cf68c7f490d6
Arturo Gutierrez <arturo1gutierrez@outlook.com>
2020-05-10 05:53:44 -0500

    Updates

---
+++ precache-manifest.bf52ea2ede6a24282852a7ea533bd76a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58078375dd46a78dad8a8e60271218a1",
+    "url": "/mamapaty/index.html"
+  },
+  {
+    "revision": "8789d4619c326fef0802",
+    "url": "/mamapaty/static/css/main.b2490a38.chunk.css"
+  },
+  {
+    "revision": "22a48e22876fe86df280",
+    "url": "/mamapaty/static/js/2.1101ddf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mamapaty/static/js/2.1101ddf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8789d4619c326fef0802",
+    "url": "/mamapaty/static/js/main.6cf4aa52.chunk.js"
+  },
+  {
+    "revision": "f2eb566b608c120f2996",
+    "url": "/mamapaty/static/js/runtime-main.aa572097.js"
+  }
+]);
